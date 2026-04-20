3b23e7b36ac55e44fd66df8d4b8b18c072a3f256
xtinacomo <ccomo2016@gmail.com>
2026-04-20 13:29:35 -0400

    Add SLURM job script for MAJIQ
    Added SLURM job configuration for MAJIQ processing.

--- symlinkcreate.sh
+++ symlinkcreate.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=majiq
+#SBATCH --nodes 1
+#SBATCH --tasks 1
+#SBATCH --cpus-per-task 16
+#SBATCH --output=./logs/majiq_%j.out
+#SBATCH --error=./logs/majiq_%j.err
+#SBATCH --time=6:00:00
+
 SRC="/data/NGD/2026_Elise_Organoid/snakemake_out/MAPPED"
 DEST="/data/NGD/2026_Elise_Organoid/snakemake_out/MAJIQ/majiq/LINKS"
 
